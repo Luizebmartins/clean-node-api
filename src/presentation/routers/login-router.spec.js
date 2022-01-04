39c6210bb7e4973cec5eeef220eba89224277a81
Luiz Martins <luizmartins.prog@gmail.com>
2022-01-04 13:41:39 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httRequest) {
-    if (!httRequest.body.email) {
+    if (!httRequest.body.email || !httRequest.body.password) {
       return {
         statusCode: 400,
       }
@@ -14,6 +14,17 @@ describe('Login router', () => {
     const httRequest = {
       body: {
         password: 'any_password',
+      },
+    }
+    const httResponse = sut.route(httRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no passwotd is provided', () => {
+    const sut = new LoginRouter()
+    const httRequest = {
+      body: {
+        email: 'any_email@mail.com',
       },
     }
     const httResponse = sut.route(httRequest)
