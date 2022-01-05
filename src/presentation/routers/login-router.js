44585d359dc460a49da19665701c8b8099fa9639
Luiz Martins <luizmartins.prog@gmail.com>
2022-01-05 14:28:08 -0300

    ensure LoginRouter return valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -21,6 +21,6 @@ module.exports = class LoginRouter {
     if (!accessToken) {
       return HttpResponse.unathorizedError()
     }
-    return HttpResponse.ok()
+    return HttpResponse.ok({ accessToken })
   }
 }
